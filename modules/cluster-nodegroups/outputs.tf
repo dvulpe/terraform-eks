fbff2982265686a66e31d81caff5bf20ea43b434
Dan Vulpe <dvulpe@gmail.com>
2020-10-10 08:22:16 +0100

    output the monitoring node group ARN so that cluster-autoscaler has permissions to scale in/out the monitoring ASG

--- modules/cluster-nodegroups/outputs.tf
+++ modules/cluster-nodegroups/outputs.tf
@@ -1,6 +1,10 @@
 output "asg_arns" {
-  value = [
-  for asg in module.zonal_workers:
-  asg.asg_arn
-  ]
+  value = concat([
+    for asg in module.zonal_workers :
+    asg.asg_arn
+    ],
+    [for asg in module.zonal_monitoring :
+      asg.asg_arn
+    ],
+  )
 }
